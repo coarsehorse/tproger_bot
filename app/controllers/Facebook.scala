3b3affb90425fdefedee4d576db15ce95552e301
heroys6 <dim.thnr@gmail.com>
2017-06-19 19:45:05 +0300

    beta

--- app/controllers/Facebook.scala
+++ app/controllers/Facebook.scala
@@ -61,11 +61,23 @@ class Facebook @Inject() (ws: WSClient) extends Controller {
 
         messageText match {
           case command("find", tag) =>
-            sendTextMessage(senderID, s"I will search in web by tag: '$tag'")
+            val art_tags = utils.Parser.searchFor(tag)
+            val str_links = art_tags map (_._1) mkString("\n")
+            sendTextMessage(senderID, s"Found articles by tag '$tag':\n"
+              + str_links + "\n"
+              + "Now writing into db ...")
+            for {
+              a_t <- art_tags
+              t <- a_t._2
+            } yield utils.DB.addNewTagArticle(t, a_t._1)
+            sendTextMessage(senderID, s"Done writing into db by tag '$tag'")
+          //sendTextMessage(senderID, s"I will search in web by tag: '$tag'")
           case command("show", tag) =>
             val articles = utils.DB.getArticlesByTag(tag)
-            sendTextMessage(senderID, s"Saved articles by tag '$tag':\n" + articles.mkString("\n"))
-            //sendTextMessage(senderID, s"I will show from db by tag: '$tag'")
+            if (articles.length == 0)
+              sendTextMessage(senderID, s"No saved articles by tag '$tag'")
+            else
+              sendTextMessage(senderID, s"Saved articles by tag '$tag':\n" + articles.mkString("\n"))
           case "help" =>
             sendTextMessage(senderID,
             """Available commands:
@@ -75,6 +87,9 @@ class Facebook @Inject() (ws: WSClient) extends Controller {
                - show saved articles by $tag
                $tag - any available tag
                satisfying re [\w\d-]+ )""")
+          case "about" =>
+            val aboutMessage = "This bot was completed within the framework of the dataRoot internship. It's open source project under the MIT license. Feel free to learn/fork/pule on Github: https://github.com/heroys6/tproger_bot"
+            sendTextMessage(senderID, aboutMessage)
           case _ =>
             sendTextMessage(senderID, "Command not recognized. Type \"help\" for more information")
         }
