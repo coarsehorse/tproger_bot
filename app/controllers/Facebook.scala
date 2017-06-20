2493e26682f0c6a2ff0cb6de870d421ae6ceba31
heroys6 <dim.thnr@gmail.com>
2017-06-21 02:52:21 +0300

    foreach to Option matching

--- app/controllers/Facebook.scala
+++ app/controllers/Facebook.scala
@@ -60,33 +60,34 @@ class Facebook @Inject() (ws: WSClient) extends Controller {
     val senderID = (event \ "sender" \ "id").as[String]
     val maybeMessage = (event \ "message").asOpt[JsObject]
 
-    maybeMessage.foreach { message =>
-      (message \ "text").asOpt[String].foreach { messageText =>
-        val command = """(\w+) tag ([\w\d-]+)""".r
+    maybeMessage match {
+      case Some(message) =>
+        (message \ "text").asOpt[String].foreach { messageText =>
+          val command = """(\w+) tag ([\w\d-]+)""".r
 
-        messageText match { // Menu
-          case command("find", tag) =>
-            val art_tags = utils.Parser.searchFor(tag)
-            val str_links = art_tags map (_._1) mkString("\n")
-            sendTextMessage(senderID, s"Found articles by tag '$tag':\n"
-              + str_links + "\n"
-              + "Now writing into db ...")
-            for {
-              a_t <- art_tags
-              t <- a_t._2
-            } yield utils.DB.addNewTagArticle(t, a_t._1)
-            sendTextMessage(senderID, s"Done writing into db by tag '$tag'")
+          messageText match { // Menu
+            case command("find", tag) =>
+              val art_tags = utils.Parser.searchFor(tag)
+              val str_links = art_tags map (_._1) mkString("\n")
+              sendTextMessage(senderID, s"Found articles by tag '$tag':\n"
+                + str_links + "\n"
+                + "Now writing into db ...")
+              for {
+                a_t <- art_tags
+                t <- a_t._2
+              } yield utils.DB.addNewTagArticle(t, a_t._1)
+              sendTextMessage(senderID, s"Done writing into db by tag '$tag'")
 
-          case command("show", tag) =>
-            val articles = utils.DB.getArticlesByTag(tag)
-            if (articles.length == 0)
-              sendTextMessage(senderID, s"No saved articles by tag '$tag'")
-            else
-              sendTextMessage(senderID, s"Saved articles by tag '$tag':\n" + articles.mkString("\n"))
+            case command("show", tag) =>
+              val articles = utils.DB.getArticlesByTag(tag)
+              if (articles.length == 0)
+                sendTextMessage(senderID, s"No saved articles by tag '$tag'")
+              else
+                sendTextMessage(senderID, s"Saved articles by tag '$tag':\n" + articles.mkString("\n"))
 
-          case "help" =>
-            sendTextMessage(senderID,
-            """Available commands:
+            case "help" =>
+              sendTextMessage(senderID,
+                """Available commands:
                find tag tagName
                  - search for articles by $tag on tproger.ru
                show tag tagName
@@ -101,14 +102,16 @@ class Facebook @Inject() (ws: WSClient) extends Controller {
                show tag scala
                   """)
 
-          case "about" =>
-            val aboutMessage = "This bot was completed within the framework of the dataRoot internship. It's open source project under the MIT license. Feel free to learn/fork/pule on Github: https://github.com/heroys6/tproger_bot"
-            sendTextMessage(senderID, aboutMessage)
+            case "about" =>
+              val aboutMessage = "This bot was completed within the framework of the dataRoot internship. It's open source project under the MIT license. Feel free to learn/fork/pule on Github: https://github.com/heroys6/tproger_bot"
+              sendTextMessage(senderID, aboutMessage)
 
-          case _ =>
-            sendTextMessage(senderID, "Command not recognized. Type \"help\" for more information")
+            case _ =>
+              sendTextMessage(senderID, "Command not recognized. Type \"help\" for more information")
+          }
         }
-      }
+      case None =>
+        // Ignore empty message
     }
   }
 
