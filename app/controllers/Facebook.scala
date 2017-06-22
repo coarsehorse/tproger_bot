a288a85b16e0f259d50c18bf9fa2e3400c459236
heroys6 <username@users.noreply.github.com>
2017-06-22 21:33:15 +0300

    Improved logic

--- app/controllers/Facebook.scala
+++ app/controllers/Facebook.scala
@@ -35,8 +35,10 @@ class Facebook @Inject() (ws: WSClient) extends Controller {
       case "page" =>
         val entry = (data \ "entry").as[JsArray]
         val messaging = (entry(0) \ "messaging").as[JsArray]
+        val senderId = (messaging(0) \ "sender" \ "id").as[String]
+        val message = (messaging(0) \ "message" \ "text").as[String]
 
-        receivedMessage(messaging(0).as[JsObject])
+        commandHandler(senderId, message)
 
         // We must send back a 200, within 20 seconds, to let Facebook know we've
         // successfully received the callback. Otherwise, the request will time out.
@@ -48,41 +50,41 @@ class Facebook @Inject() (ws: WSClient) extends Controller {
   }
 
   /**
-    * Handle user message
-    * @param event user message with auxiliary data
+    * Handles user command from message
+    * @param senderId user id
+    * @param message user message
     */
-  private def receivedMessage(event: JsObject) = {
-    val senderID = (event \ "sender" \ "id").as[String]
-    val maybeMessage = (event \ "message").asOpt[JsObject]
+  private def commandHandler(senderId: String, message: String) = {
+    val command = """(\w+) tag ([\w\d-]+)""".r
 
-    maybeMessage match {
-      case Some(message) =>
-        (message \ "text").asOpt[String].foreach { messageText =>
-          val command = """(\w+) tag ([\w\d-]+)""".r
+    message match {
+      case command("find", tag) =>
+        val art_tags = utils.Parser.searchFor(tag)
 
-          messageText match { // Menu
-            case command("find", tag) =>
-              val art_tags = utils.Parser.searchFor(tag)
-              val str_links = art_tags map (_._1) mkString("\n")
-              sendTextMessage(senderID, s"Found articles by tag '$tag':\n"
-                + str_links + "\n"
-                + "Now writing into db ...")
-              for {
-                a_t <- art_tags
-                t <- a_t._2
-              } yield utils.DB.addNewTagArticle(t, a_t._1)
-              //sendTextMessage(senderID, s"Done writing into db by tag '$tag'")
+        if (art_tags.length == 0)
+          sendTextMessage(senderId, s"No articles by tag '$tag' was found on tproger.ru")
+        else {
+          val str_links = art_tags map (_._1) mkString("\n")
+          sendTextMessage(senderId,
+            s"Found articles by tag '$tag':\n"
+            + str_links + "\n"
+            + "Now writing into db ...")
+          for {
+            a_t <- art_tags
+            t <- a_t._2
+          } yield utils.DB.addNewTagArticle(t, a_t._1)
+        }
 
-            case command("show", tag) =>
-              val articles = utils.DB.getArticlesByTag(tag)
-              if (articles.length == 0)
-                sendTextMessage(senderID, s"No saved articles by tag '$tag'")
-              else
-                sendTextMessage(senderID, s"Saved articles by tag '$tag':\n" + articles.mkString("\n"))
+      case command("show", tag) =>
+        val articles = utils.DB.getArticlesByTag(tag)
+        if (articles.length == 0)
+          sendTextMessage(senderId, s"No saved articles by tag '$tag'")
+        else
+          sendTextMessage(senderId, s"Saved articles by tag '$tag':\n" + articles.mkString("\n"))
 
-            case "help" =>
-              sendTextMessage(senderID,
-                """Available commands:
+      case "help" =>
+        sendTextMessage(senderId,
+          """Available commands:
                find tag tagValue
                  - search by tagValue on tproger.ru
                show tag tagValue
@@ -97,16 +99,12 @@ class Facebook @Inject() (ws: WSClient) extends Controller {
                show tag scala
                   """)
 
-            case "about" =>
-              val aboutMessage = "This bot was completed within the framework of the dataRoot internship. It's open source project under the MIT license. Feel free to learn/fork/pule on Github: https://github.com/heroys6/tproger_bot"
-              sendTextMessage(senderID, aboutMessage)
+      case "about" =>
+        val aboutMessage = "This bot was completed within the framework of the dataRoot internship. It's open source project under the MIT license. Feel free to learn/fork/pule on Github: https://github.com/heroys6/tproger_bot"
+        sendTextMessage(senderId, aboutMessage)
 
-            case _ =>
-              sendTextMessage(senderID, "Command not recognized. Type \"help\" for more information")
-          }
-        }
-      case None =>
-        // Ignore empty message
+      case _ =>
+        sendTextMessage(senderId, "Command not recognized. Type \"help\" for more information")
     }
   }
 
